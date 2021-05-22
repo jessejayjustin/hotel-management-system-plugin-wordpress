66c214cbef323d662398beedc9954d765801d339
Jesse Sosmena <23520011+jessejayjustin@users.noreply.github.com>
2021-05-22 22:50:09 +0800

    Add rooms sub menu in admin panel for creating rooms

--- admin/rooms.php
+++ admin/rooms.php
@@ -2,24 +2,24 @@
 	<div class="col-lg-12">
 		<div class="row">
 			<!-- FORM Panel -->
-			<div class="col-md-4">
+			<div class="col-md-8">
 			<form action="" id="manage-room">
 				<div class="card">
 					<div class="card-header">
 						    Room Form
 				  	</div>
 					<div class="card-body">
-							<input type="hidden" name="id">
+							<input type="hidden" id="id" name="id">
 							<div class="form-group">
 								<label class="control-label">Room</label>
-								<input type="text" class="form-control" name="room">
+								<input type="text" class="form-control" id="room" name="room">
 							</div>
 							<div class="form-group">
 								<label class="control-label">Category</label>
-								<select class="custom-select browser-default" name="category_id">
+								<select class="custom-select browser-default" id="category_id" name="category_id">
 									<?php 
-									$cat = $conn->query("SELECT * FROM room_categories order by name asc ");
-									while($row= $cat->fetch_assoc()) {
+									$cat = $wpdb->get_results("SELECT * FROM wp_room_categories order by name asc", ARRAY_A);
+									foreach($cat as $row) {
 										$cat_name[$row['id']] = $row['name'];
 										?>
 										<option value="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></option>
@@ -30,7 +30,7 @@
 							</div>
 							<div class="form-group">
 								<label for="" class="control-label">Availability</label>
-								<select class="custom-select browser-default" name="status">
+								<select class="custom-select browser-default" id="status" name="status">
 									<option value="0">Available</option>
 									<option value="1">Unavailable</option>
 
@@ -42,7 +42,7 @@
 						<div class="row">
 							<div class="col-md-12">
 								<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Save</button>
-								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="$('#manage-room').get(0).reset()"> Cancel</button>
+								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="$j('#manage-room').get(0).reset()"> Cancel</button>
 							</div>
 						</div>
 					</div>
@@ -51,8 +51,13 @@
 			</div>
 			<!-- FORM Panel -->
 
+			<!-- Modal -->
+			<?php
+			   include($_SERVER['DOCUMENT_ROOT'] .'/wordpress/wp-content/plugins/hotel-booking/admin/modal.php');
+			?>
+
 			<!-- Table Panel -->
-			<div class="col-md-8">
+			<div class="col-md-12">
 				<div class="card">
 					<div class="card-body">
 						<table class="table table-bordered table-hover">
@@ -68,13 +73,12 @@
 							<tbody>
 								<?php 
 								$i = 1;
-								$rooms = $conn->query("SELECT * FROM rooms order by id asc");
-								while($row=$rooms->fetch_assoc()):
-								?>
+								$rooms = $wpdb->get_results("SELECT * FROM wp_rooms order by id asc", ARRAY_A);
+								foreach($rooms as $row) {
+                                ?>
 								<tr>
 									<td class="text-center"><?php echo $i++ ?></td>
-
-								
+			
 									<td class="text-center"><?php echo $cat_name[$row['category_id']] ?></td>
 									<td class=""><?php echo $row['room'] ?></td>
 									<?php if($row['status'] == 0): ?>
@@ -84,10 +88,12 @@
 									<?php endif; ?>
 									<td class="text-center">
 										<button class="btn btn-sm btn-primary edit_cat" type="button" data-id="<?php echo $row['id'] ?>" data-room="<?php echo $row['room'] ?>" data-category_id="<?php echo $row['category_id'] ?>" data-status="<?php echo $row['status'] ?>">Edit</button>
-										<button class="btn btn-sm btn-danger delete_cat" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+										<button style="margin-top: 6px;" class="btn btn-sm btn-danger delete_cat" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
 									</td>
 								</tr>
-								<?php endwhile; ?>
+								<?php 
+                                  }
+								?>
 							</tbody>
 						</table>
 					</div>
@@ -96,63 +102,88 @@
 			<!-- Table Panel -->
 		</div>
 	</div>	
-
 </div>
 
 <script>
-	$('#manage-room').submit(function(e){
-		e.preventDefault()
-		start_load()
-		$.ajax({
-			url:'ajax.php?action=save_room',
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+	$j('#manage-room').submit(function(e) {
+		e.preventDefault();
+		start_load();
+		$j.ajax({
+			url:'http://localhost/wordpress/wp-content/plugins/hotel-booking/admin/save_room.php?action=save_room',
 			method:"POST",
-			data: $(this).serialize(),
+			data:
+			{
+		        id: $j("#id").val(),
+		        room: $j("#room").val(),
+		        category_id: $j("#category_id").val(),
+		        status: $j("#status").val()
+		    },
 			success:function(resp){
+				console.log(resp);
 				if(resp==1){
-					alert_toast("Data successfully added",'success')
+					alert_toast("Data successfully added",'success');
 					setTimeout(function(){
 						location.reload()
-					},1500)
-
+					},1500);
 				}
 				else if(resp==2){
-					alert_toast("Data successfully updated",'success')
+					alert_toast("Data successfully updated",'success');
 					setTimeout(function(){
 						location.reload()
-					},1500)
-
+					},1500);
 				}
 			}
 		})
-	})
-	$('.edit_cat').click(function(){
-		start_load()
-		var cat = $('#manage-room')
-		cat.get(0).reset()
-		cat.find("[name='id']").val($(this).attr('data-id'))
-		cat.find("[name='room']").val($(this).attr('data-room'))
-		cat.find("[name='category_id']").val($(this).attr('data-category_id'))
-		cat.find("[name='status']").val($(this).attr('data-status'))
-		end_load()
-	})
-	$('.delete_cat').click(function(){
-		_conf("Are you sure to delete this room?","delete_cat",[$(this).attr('data-id')])
-	})
+	});
+	$j('.edit_cat').click(function(){
+		start_load();
+		var cat = $j('#manage-room');
+		cat.get(0).reset();
+		cat.find("[name='id']").val($j(this).attr('data-id'));
+		cat.find("[name='room']").val($j(this).attr('data-room'));
+		cat.find("[name='category_id']").val($j(this).attr('data-category_id'));
+		cat.find("[name='status']").val($j(this).attr('data-status'));
+		end_load();
+	});
+	$j('.delete_cat').click(function(){
+		var id = $j(this).attr('data-id');
+		$j('#confirm_modal .modal-body').html("Are you sure to delete this category?")
+	    $j('#confirm_modal').modal('show');
+        confirm_del(id);
+	});
+	function confirm_del($id) {
+		$j('#confirm').click(function(){
+			delete_cat($id);
+			$j('#confirm_modal').modal('hide');
+		});
+    }
 	function delete_cat($id){
-		start_load()
-		$.ajax({
-			url:'ajax.php?action=delete_room',
+		start_load();
+		$j.ajax({
+			url:'http://localhost/wordpress/wp-content/plugins/hotel-booking/admin/delete_room.php?action=delete_room',
 			method:'POST',
 			data:{id:$id},
 			success:function(resp){
 				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
 					setTimeout(function(){
 						location.reload()
-					},1500)
+					},1500);
 
 				}
 			}
-		})
+		});
 	}
+	
+	window.start_load = function(){
+       $j('body').prepend('<div id="preloader2"></div>');
+    }
+    window.end_load = function(){
+        $j('#preloader2').fadeOut('fast', function() {
+          $j(this).remove();
+        });
+    }
+});
 </script>
